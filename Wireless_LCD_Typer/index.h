616ae210c9c75174d7080bd5fb0887a24c1434d7
Linda TJ <sameer.tuteja05@gmail.com>
2020-10-20 17:14:38 +0530

    Add files via upload

---
+++ Wireless_LCD_Typer/index.h
@@ -0,0 +1,51 @@
+//const char MAIN_page[] PROGMEM = R"=====(
+//<!DOCTYPE html>
+//<HTML>
+//  <HEAD>
+//      <TITLE>My first web page</TITLE>
+//  </HEAD>
+//<BODY>
+//  <CENTER>
+//      <B>Hello World.... </B>
+//  </CENTER> 
+//</BODY>
+//</HTML>
+//)=====";
+
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title> Wireless LCD Input Form </title>    
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
+  </head>
+  <body>
+  Text auto updates in 2 seconds.<br><br>
+  Message deletes after 10 seonds<br><br>  
+  <form id="myForm" action="/get">
+    Your Text: <input type="text" name="val" autofocus autocomplete="off" maxlength="31" onkeyup="myFunction()">
+    <!-- <input id="fname" type="button" onclick="myFunction()" value="Submit"> -->
+  <input type="button" onclick="myFunction()" value="Submit">
+  </form>
+  <br>  
+  <script> 
+  function myFunction() {          
+  try{
+  console.log('clear timeout');  
+  clearTimeout(myVar);
+  }
+  catch(e){}
+  myVar = setTimeout(
+  function(){
+  console.log('in timeout');
+  document.getElementById("myForm").submit();  
+  done=true;  
+  }
+  ,2000); 
+  console.log('got here');  
+  }
+  </script> 
+  </body>
+</html>
+)rawliteral";
